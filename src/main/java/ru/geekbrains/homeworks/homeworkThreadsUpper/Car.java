960b6307d8bb79a3d3d946d3c482230b295f4e57
Kirill Korsuk <korsukkirill@gmail.com>
2022-02-25 21:45:15 +0300

    homework concurrent

--- src/main/java/ru/geekbrains/homeworks/homeworkThreadsUpper/Car.java
+++ src/main/java/ru/geekbrains/homeworks/homeworkThreadsUpper/Car.java
@@ -1,10 +1,14 @@
 package ru.geekbrains.homeworks.homeworkThreadsUpper;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     private Race race;
     private int speed;
     private String name;
+
     public String getName() {
         return name;
     }
@@ -17,18 +21,33 @@ public class Car implements Runnable {
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
     }
+
     @Override
     public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
+            MainClass.barrier.await();
             System.out.println(this.name + " готов");
+            MainClass.start.countDown();
+            MainClass.barrier.await();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        MainClass.finish.countDown();
+        if (MainClass.lock.tryLock()) {
+            System.out.println(this.getName() + " - WIN!");
+        }
+
+    }
+
+    public Thread getThread() {
+        return Thread.currentThread();
     }
 }
 
--- src/main/java/ru/geekbrains/homeworks/homeworkThreadsUpper/MainClass.java
+++ src/main/java/ru/geekbrains/homeworks/homeworkThreadsUpper/MainClass.java
@@ -1,19 +1,49 @@
 package ru.geekbrains.homeworks.homeworkThreadsUpper;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
+   public static CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT);
+    public static CountDownLatch start = new CountDownLatch(CARS_COUNT);
+    public static CountDownLatch finish = new CountDownLatch(CARS_COUNT);
+    public static Lock lock = new ReentrantLock();
+    public static Semaphore semaphore = new Semaphore(CARS_COUNT / 2, true);
     public static void main(String[] args) {
+
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+
+        Race race = new Race(new Road(60), new Tunnel(semaphore), new Road(40));
+
         Car[] cars = new Car[CARS_COUNT];
+
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
         }
+
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+                new Thread(cars[i]).start();}
+
+        try {
+            start.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+        try {
+            finish.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        }
     }
 
-}
+
